e18a3ab2ee293a9fa2b02f887e5a96a41013b32c
bholekiran10 <130656290+bholekiran10@users.noreply.github.com>
2023-04-13 20:51:44 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,12 +11,12 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     def read(self, path):
-        with open(path, 'r') as reader1:
-            return reader1.read()  
+        with open(path, "r") as file:
+            return file.read() 
     def write(self, path, dest, content, ext =".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path, 'w') as writer:
-            writer.write(content)     
+        with open(full_path, "w") as file:
+            file.write(content)    
     def copy(self, path, source, dest):
         shutil.copy2(path, (dest / path.relative_to(source)))
 
