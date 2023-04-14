f289abb15d80df21fa5dd38b3c3317710ad530cb
bholekiran10 <130656290+bholekiran10@users.noreply.github.com>
2023-04-14 09:33:41 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -4,7 +4,7 @@ from collections.abc import Mapping
 
 class Content(Mapping):
     __delimeter = r"^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimiter, re.MULTILINE)
+    __regex = re.compile(__delimeter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
