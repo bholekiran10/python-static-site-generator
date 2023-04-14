d56838f86e77a872e483af0adcfd5ea3ec0252bf
bholekiran10 <130656290+bholekiran10@users.noreply.github.com>
2023-04-14 10:19:41 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -9,10 +9,10 @@ class Content(Mapping):
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        metadata = load(fm, Loader = FullLoader)
-        return cls(metadata,  content)        
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)        
     def __init__(self, metadata, content):
-        data = metadata
+        self.data = metadata
         self.data["content"] = content
     @property
     def body(self):
